2bc379b21174f4c8ad0e4398af0141090ad93ff7
Dilmer Valecillos ᯅ <dilmervst@gmail.com>
2025-09-06 17:36:47 -0600

    Initial project files

---
+++ Assets/Scripts/TrackablesManager.cs
@@ -0,0 +1,42 @@
+using LearnXR.Core.Utilities;
+using Meta.XR.MRUtilityKit;
+using TMPro;
+using UnityEngine;
+
+public class TrackablesManager : MonoBehaviour
+{
+    [SerializeField] private GameObject trackedObjectPrefab;
+    [SerializeField] private GameObject trackerInfoPrefab;
+    [SerializeField] private GameObject trackerBounds;
+    
+    public void OnTrackableAdded(MRUKTrackable trackable)
+    {
+        SpatialLogger.Instance.LogInfo($"Trackable of type {trackable.TrackableType} added");
+        var trackedObjectInstance = Instantiate(trackedObjectPrefab, trackable.transform);
+        
+        var trackedBoundsInstance = Instantiate(trackerBounds, trackedObjectInstance.transform);
+        var boundingAreaRect = trackable.PlaneRect.Value;
+        
+        if (trackable.TrackableType == OVRAnchor.TrackableType.QRCode 
+            && trackable.MarkerPayloadString != null)
+        {
+            var trackedObjectInfo = Instantiate(trackerInfoPrefab, trackedObjectInstance.transform);
+            trackedObjectInfo.GetComponentInChildren<TextMeshProUGUI>()
+                    .text = $"<color=red>QR Code Payload:</color> {trackable.MarkerPayloadString}";
+            
+            trackedBoundsInstance.transform.localScale = new Vector3(boundingAreaRect.width, boundingAreaRect.height, 0.01f);
+            trackedBoundsInstance.transform.localPosition = new Vector3(boundingAreaRect.center.x, boundingAreaRect.center.y, 0f);
+        }
+        else if(trackable.TrackableType == OVRAnchor.TrackableType.Keyboard)
+        {
+            trackedBoundsInstance.transform.localScale = trackable.VolumeBounds.Value.size;
+            trackedBoundsInstance.transform.localPosition = Vector3.zero;
+        }
+    }
+
+    public void OnTrackableRemoved(MRUKTrackable trackable)
+    {
+        SpatialLogger.Instance.LogInfo($"Trackable removed: {trackable.name}");
+        Destroy(trackable.gameObject);
+    }
+}
